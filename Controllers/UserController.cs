d3a4993798969b1eeaaa7ee439782a8c796fc9e3
marina-barbosa <marinabarbosa.exp@gmail.com>
2024-11-12 02:02:33 -0300

    feat: add UpdateUser

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using make_it_happen.Context;
 using make_it_happen.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace make_it_happen.Controllers;
 
@@ -35,5 +36,26 @@ public class UserController(AppDbContext context) : ControllerBase
     _context.Users!.Add(user);
     _context.SaveChanges();
     return new CreatedAtRouteResult("GetUserById", new { id = user.UserId }, user);
+  }
+
+  [HttpPut("{id:int}")]
+  public ActionResult<User> UpdateUser(int id, User user)
+  {
+    if(id != user.UserId) return BadRequest("O id informado não pode ser diferente do id do usuário");
+
+    var userToUpdate = _context.Users!.FirstOrDefault(x => x.UserId == id);
+    if (userToUpdate == null) return NotFound("Usuário nao encontrado");
+
+    userToUpdate.Name = user.Name;
+    userToUpdate.Email = user.Email;
+    userToUpdate.Password = user.Password;
+    userToUpdate.AvatarUrl = user.AvatarUrl;
+    userToUpdate.Bio = user.Bio;
+    userToUpdate.Contact = user.Contact;
+    userToUpdate.Status = user.Status;
+    userToUpdate.EmailVerified = user.EmailVerified;
+
+    _context.SaveChanges();
+    return Ok(user);
   }
 }
